16f675dbfe54a93323962a455133e9bb6fe562de
Luuka Lindgren <luslin@utu.fi>
2023-04-03 12:51:27 +0300

    Responding to messages

--- bot.py
+++ bot.py
@@ -5,11 +5,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user.name} is connected to:\n' 
+        f'{guild.name} (id: {guild.id})'
+    )
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    await message.channel.send('Biip boop')
 
 client.run(TOKEN)
